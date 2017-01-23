5e7401a3f2cbf1cd1faa1aa9c0cc8c36629679c0
lai yin yeung <dvd042002@gmail.com>
2017-01-23 02:57:08 +0000

    initial commit, set up Unity project, set up skysphere, set up camera mouse drag rotation + wheel zoom

---
+++ Vincent/Assets/FirstPersonCam.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCam : MonoBehaviour
+{
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    public float minFov = 15f;
+    public float maxFov = 90f;
+    public float sensitivity = 10f;
+
+    private float zoomSpeed = 200.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    private float fov;
+    
+
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+
+            yaw -= speedH * Input.GetAxis("Mouse X");
+            pitch += speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+        }
+
+        fov = Camera.main.fieldOfView;
+        fov -= Input.GetAxis("Mouse ScrollWheel") * sensitivity;
+        fov = Mathf.Clamp(fov, minFov, maxFov);
+        Camera.main.fieldOfView = fov;
+
+// oat scroll = Input.GetAxis("Mouse ScrollWheel");
+ //    transform.Translate(0, scroll * zoomSpeed, scroll * zoomSpeed, Space.World);
+    }
+}
