203f6402671e764a7c83f037139d3581e08c1bb2
lai yin yeung <dvd042002@gmail.com>
2017-05-17 22:50:09 +0100

    Added button to switch between starmap and constellation mode

---
+++ Vincent/Assets/SwitchBGmode.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwitchBGmode : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject starmapSphere;
+    public GameObject constellationSphere;
+
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        starmapSphere.SetActive(!starmapSphere.activeSelf);
+        constellationSphere.SetActive(!constellationSphere.activeSelf);
+    }
+}
