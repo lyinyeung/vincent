3f64f490392b7b8640f53c9dec9c7f272740f2df
lai yin yeung <dvd042002@gmail.com>
2017-05-17 20:05:57 +0100

    Added assets and scripts to draw celestial grid lines

---
+++ Vincent/Assets/InstantiateSphereGrid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateSphereGrid : MonoBehaviour {
+
+    public Transform linePrefab;
+
+    // Use this for initialization
+    void Start () {
+		for (int i = 0; i < 12; i++)
+        {
+            Instantiate(linePrefab, new Vector3(0, 0, 0), Quaternion.identity).Rotate(new Vector3(0, 0 + (15 * i), 0), Space.Self);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            var lineH = Instantiate(linePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            lineH.Rotate(new Vector3(90, 0), Space.Self);
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
