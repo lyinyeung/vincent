fe8e77d661e45307018258da6d47fb16dccada24
lai yin yeung <dvd042002@gmail.com>
2017-05-24 15:50:08 +0100

    Completed basic computation for sun, moon and planets

--- Vincent/Assets/SolarSystemCalculations.cs
+++ Vincent/Assets/SolarSystemCalculations.cs
@@ -7,12 +7,24 @@ using UnityEngine.UI;
 public class SolarSystemCalculations : MonoBehaviour {
 
     public Text t;
+    public Transform mainCam;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject sunObj;
+    public GameObject moonObj;
+    public GameObject mercuryObj;
+    public GameObject venusObj;
+    public GameObject marsObj;
+    public GameObject jupiterObj;
+    public GameObject saturnObj;
+    public GameObject uranusObj;
+    public GameObject neptuneObj;
 
-        int d = dayNumber(1990,4,19);
+    // Use this for initialization
+    void Start () {
+
+        int d = dayNumber(2012,5,21);
         Sun sun = new Sun(d);
+        Moon moon = new Moon(d);
 
         Planet mercury = new Planet(
             "Mercury",
@@ -24,6 +36,29 @@ public class SolarSystemCalculations : MonoBehaviour {
             168.6562, 4.0923344368,
             sun,
             d);
+
+        Planet venus = new Planet(
+           "Venus",
+           76.6799, 2.46590 * Mathf.Pow(10, -5F),
+           3.3946, 2.75 * Mathf.Pow(10, -8F),
+           54.8910, 1.38374 * Mathf.Pow(10, -5F),
+           0.723330,
+           0.006773, -1.302 * Mathf.Pow(10, -9F),
+           48.0052, 1.6021302244,
+           sun,
+           d);
+
+        Planet mars = new Planet(
+          "Mars",
+          49.5574, 2.11081 * Mathf.Pow(10, -5F),
+          1.8497, -1.78 * Mathf.Pow(10, -8F),
+          286.5016, 2.92961 * Mathf.Pow(10, -5F),
+          1.523688,
+          0.093405, 2.516 * Mathf.Pow(10, -9F),
+          18.6021, 0.5240207766,
+          sun,
+          d);
+
         Planet jupiter = new Planet(
             "Jupiter",
             100.4542, 2.76854 * Mathf.Pow(10, -5F),
@@ -34,6 +69,7 @@ public class SolarSystemCalculations : MonoBehaviour {
             19.8950, 0.0830853001,
             sun,
             d);
+
         Planet saturn = new Planet(
             "Saturn",
             113.6634, 2.38980 * Mathf.Pow(10, -5F),
@@ -44,6 +80,7 @@ public class SolarSystemCalculations : MonoBehaviour {
             316.9670, 0.0334442282,
             sun,
             d);
+
         Planet uranus = new Planet(
             "Uranus",
             74.0005, 1.3978 * Mathf.Pow(10, -5F),
@@ -54,7 +91,46 @@ public class SolarSystemCalculations : MonoBehaviour {
             142.5905, 0.011725806,
             sun,
             d);
-        t.text = (uranus.coords.ra).ToString();
+
+        Planet neptune = new Planet(
+         "Neptune",
+         131.7806, 3.0173 * Mathf.Pow(10, -5F),
+         1.7700, -2.55 * Mathf.Pow(10, -7F),
+         272.8461, -6.027 * Mathf.Pow(10, -6F),
+         30.05826,
+         0.008606, 2.15 * Mathf.Pow(10, -9F),
+         260.2471, 0.005995147,
+         sun,
+         d);
+        t.text = (sun.coords.ra).ToString();
+
+        for (int i = 0; i < 8; i++)
+        {
+
+        }
+        
+        sunObj.transform.position     = sph2Cart(sun.coords.ra, sun.coords.dec, 499 + sun.dist/100);
+        moonObj.transform.position    = sph2Cart(moon.coords.ra, moon.coords.dec, 499);
+        mercuryObj.transform.position = sph2Cart(mercury.coords.ra, mercury.coords.dec, 499 + mercury.dist/100);
+        venusObj.transform.position   = sph2Cart(venus.coords.ra, venus.coords.dec, 499 + venus.dist / 100);
+        marsObj.transform.position    = sph2Cart(mars.coords.ra, mars.coords.dec, 499 + mars.dist / 100);
+        jupiterObj.transform.position = sph2Cart(jupiter.coords.ra, jupiter.coords.dec, 499 + jupiter.dist / 100);
+        saturnObj.transform.position  = sph2Cart(saturn.coords.ra, saturn.coords.dec, 499 + saturn.dist / 100);
+        uranusObj.transform.position  = sph2Cart(uranus.coords.ra, uranus.coords.dec, 499 + uranus.dist / 100);
+        neptuneObj.transform.position = sph2Cart(neptune.coords.ra, neptune.coords.dec, 499 + neptune.dist / 100);
+
+        sunObj.transform.LookAt(mainCam);
+        moonObj.transform.LookAt(mainCam);
+        mercuryObj.transform.LookAt(mainCam);
+        venusObj.transform.LookAt(mainCam);
+        marsObj.transform.LookAt(mainCam);
+        jupiterObj.transform.LookAt(mainCam);
+        saturnObj.transform.LookAt(mainCam);
+        uranusObj.transform.LookAt(mainCam);
+        neptuneObj.transform.LookAt(mainCam);
+
+        Debug.Log(sun.coords.ra.ToString());
+
     }
 
     public struct Coords
@@ -77,6 +153,8 @@ public class SolarSystemCalculations : MonoBehaviour {
         public double x;    
         public double y;
         public double z;
+        public double dist;
+
 
         public Sun (int d)
         {
@@ -103,9 +181,104 @@ public class SolarSystemCalculations : MonoBehaviour {
             double zEq = y * Mathf.Sin((float) D2R(o)) + z * Mathf.Cos((float) D2R(o));
             double ra = R2D(Mathf.Atan2((float)yEq, (float)xEq));
             double dec = R2D(Mathf.Atan2((float)zEq, Mathf.Sqrt((float)(xEq * xEq + yEq * yEq))));
+            dist = r;
             coords = new Coords(ra, dec);
         }
     }
+
+    public struct Moon
+    {
+        public Coords coords;
+
+        // in degrees
+        public double n;      // Long of asc. node
+        public double i;      // Inclination 
+        public double w;      // Argument of perihelion
+        public double a;      // Semi-major axis
+        public double e;      // Eccentricity
+        public double M;      // Mean anomoaly
+
+
+
+        public Moon (int d)
+        {
+            n = Rev(125.1228 - 0.0529538083 * d);
+            i = Rev(5.1454);
+            w = Rev(318.0634 + 0.1643573223 * d);
+            a = 60.2666;
+            e = 0.054900;
+            M = Rev(115.3654 + 13.0649929509 * d);
+
+            double eAnom = EAnomIteration(e, M);
+
+            // Lunar orbit coords
+            double xEq = a * (Mathf.Cos((float)D2R(eAnom)) - e);
+            double yEq = a * (Mathf.Sin((float)D2R(eAnom)) * Mathf.Sqrt((float)(1 - e * e)));
+
+            double r = Mathf.Sqrt((float)(xEq * xEq + yEq * yEq)); // distance
+            double v = R2D(Mathf.Atan2((float)yEq, (float)xEq));   // True anomaly
+
+            double xEcl = r * (Mathf.Cos((float)D2R(n)) * Mathf.Cos((float)D2R((v + w))) - Mathf.Sin((float)D2R(n))
+                * Mathf.Sin((float)D2R((v + w))) * Mathf.Cos((float)D2R(i)));
+            double yEcl = r * (Mathf.Sin((float)D2R(n)) * Mathf.Cos((float)D2R((v + w))) + Mathf.Cos((float)D2R(n))
+                * Mathf.Sin((float)D2R((v + w))) * Mathf.Cos((float)D2R(i)));
+            double zEcl = r * Mathf.Sin((float)D2R((v + w))) * Mathf.Sin((float)D2R((i)));
+
+
+            double lon = Rev(R2D(Mathf.Atan2((float)yEcl, (float)xEcl)));
+            double lat = Rev(R2D(Mathf.Atan2((float)zEcl, Mathf.Sqrt((float)(xEcl * xEcl + yEcl * yEcl)))));
+
+            double lS = Rev((282.9404 + 4.70935 * Mathf.Pow(10, -5F) * d) + Rev(356.0470 + 0.9856002585 * d)); // Sun mean longitude
+            double lM = Rev(n + w + M); // Moon mean longitude
+            double mS = Rev(356.0470 + 0.9856002585 * d); // Sun mean anomaly
+            double elong = lM - lS; // Moon mean elongation
+            double f = lM - n; // Moon argument of latitude
+
+            // Pertubations
+            
+            lon += -1.274 * Mathf.Sin((float)D2R(M - 2 * elong))
+                + 0.658 * Mathf.Sin((float)D2R(2 * elong))
+                - 0.186 * Mathf.Sin((float)D2R(mS))
+                - 0.059 * Mathf.Sin((float)D2R(2 * M - 2 * elong))
+                - 0.057 * Mathf.Sin((float)D2R(M - 2 * elong + mS))
+                + 0.053 * Mathf.Sin((float)D2R(M + 2 * elong))
+                + 0.046 * Mathf.Sin((float)D2R(2 * elong - mS))
+                + 0.041 * Mathf.Sin((float)D2R(M - mS))
+                - 0.035 * Mathf.Sin((float)D2R(elong))
+                - 0.031 * Mathf.Sin((float)D2R(M + mS))
+                - 0.015 * Mathf.Sin((float)D2R(2 * f - 2 * elong))
+                + 0.011 * Mathf.Sin((float)D2R(M - 4 * elong));
+
+            lat += -0.173 * Mathf.Sin((float)D2R(f - 2 * elong))
+                - 0.055 * Mathf.Sin((float)D2R(M - f - 2 * elong))
+                - 0.046 * Mathf.Sin((float)D2R(M + f - 2 * elong))
+                + 0.033 * Mathf.Sin((float)D2R(f + 2 * elong))
+                + 0.017 * Mathf.Sin((float)D2R(2 * M + f));
+
+            r += -0.58 * Mathf.Cos((float)D2R(M - 2 * elong))
+                - 0.46 * Mathf.Cos((float)D2R(2 * elong));
+
+            xEcl = r * Mathf.Cos((float)D2R(lon)) * Mathf.Cos((float)D2R(lat));
+            yEcl = r * Mathf.Sin((float)D2R(lon)) * Mathf.Cos((float)D2R(lat));
+            zEcl = r * Mathf.Sin((float)D2R(lat));
+            
+            double oblec = 23.4406;
+            double xGeoRot = xEcl;
+            double yGeoRot = yEcl * Mathf.Cos((float)D2R(oblec)) - zEcl * Mathf.Sin((float)D2R(oblec));
+            double zGeoRot = yEcl * Mathf.Sin((float)D2R(oblec)) + zEcl * Mathf.Cos((float)D2R(oblec));
+
+
+            double ra = Rev(R2D(Mathf.Atan2((float)yGeoRot, (float)xGeoRot)));
+            double dec = R2D(Mathf.Atan2((float)zGeoRot, Mathf.Sqrt((float)(xGeoRot * xGeoRot + yGeoRot * yGeoRot))));
+            double dist = Mathf.Sqrt((float)(xGeoRot * xGeoRot + yGeoRot * yGeoRot + zGeoRot * zGeoRot));
+
+            
+
+            coords = new Coords(ra, dec);
+        }
+
+    }
+
 
     public struct Planet
     {
@@ -119,6 +292,7 @@ public class SolarSystemCalculations : MonoBehaviour {
         public double e;      // Eccentricity
         public double M;      // Mean anomoaly
         public Coords coords; // RA and Dec
+        public double dist;   // Distance from Earth
 
         public Planet(
             string name,
@@ -139,6 +313,10 @@ public class SolarSystemCalculations : MonoBehaviour {
             if (name == "Uranus")
             {
                 a += -1.55 * Mathf.Pow(10,-8) * d;
+            }
+            if (name == "Neptune")
+            {
+                a += 3.313 * Mathf.Pow(10, -8) * d;
             }
             e = e1 + e2 * d;
             M = M1 + M2 * d;
@@ -162,12 +340,11 @@ public class SolarSystemCalculations : MonoBehaviour {
             // Heliocentric spherical coords
             double lon = Rev(R2D(Mathf.Atan2((float)yEcl, (float)xEcl)));
             double lat = Rev(R2D(Mathf.Atan2((float)zEcl, Mathf.Sqrt((float)(xEcl * xEcl + yEcl * yEcl)))));
-
+            
             // Pertubations calculations for Jupiter, Saturn & Uranus
             double mJ = Rev(19.8950 + 0.0830853001 * d);   // Mean anomalies
             double mS = Rev(316.9670 + 0.0334442282 * d);
             double mU = Rev(142.5905 + 0.011725806 * d);
-            
             if (name == "Jupiter")
             {
                 lon += -0.332 * Mathf.Sin((float)D2R(2 * mJ - 5 * mS - 67.6))
@@ -178,7 +355,6 @@ public class SolarSystemCalculations : MonoBehaviour {
                     + 0.023 * Mathf.Sin((float)D2R(2 * mJ - 3 * mS + 52))
                     - 0.016 * Mathf.Sin((float)D2R(mJ - 5 * mS - 69));
             }
-            
             if (name == "Saturn")
             {
                 lon += 0.812 * Mathf.Sin((float)D2R(2 * mJ - 5 * mS - 67.6))
@@ -189,7 +365,7 @@ public class SolarSystemCalculations : MonoBehaviour {
 
                 lat += -0.020 * Mathf.Cos((float)D2R(2 * mJ - 4 * mS - 2))
                     +0.018 * Mathf.Sin((float)D2R(2 * mJ - 6 * mS - 49));
-
+                
             }
             if (name == "Uranus")
             {
@@ -198,6 +374,10 @@ public class SolarSystemCalculations : MonoBehaviour {
                     - 0.015 * Mathf.Sin((float)D2R(mJ - mU + 20));
             }
             
+            xEcl = r * Mathf.Cos((float)D2R(lon)) * Mathf.Cos((float)D2R(lat));
+            yEcl = r * Mathf.Sin((float)D2R(lon)) * Mathf.Cos((float)D2R(lat));
+            zEcl = r * Mathf.Sin((float)D2R(lat));
+
             // Geocentric coords
             double xGeo = xEcl + sun.x;
             double yGeo = yEcl + sun.y;
@@ -208,15 +388,13 @@ public class SolarSystemCalculations : MonoBehaviour {
             double yGeoRot = yGeo * Mathf.Cos((float) D2R(oblec)) - zGeo * Mathf.Sin((float) D2R(oblec));
             double zGeoRot = yGeo * Mathf.Sin((float) D2R(oblec)) + zGeo * Mathf.Cos((float) D2R(oblec));
             
-
             // Geocentric spherical coords
-            double ra  = R2D(Mathf.Atan2((float)yGeoRot, (float)xGeo));
+            double ra  = Rev(R2D(Mathf.Atan2((float)yGeoRot, (float)xGeo)));
             double dec = R2D(Mathf.Atan2((float)zGeoRot, Mathf.Sqrt((float)(xGeo * xGeo + yGeoRot * yGeoRot))));
-            double dist = Mathf.Sqrt((float) (xGeo * xGeo + yGeoRot * yGeoRot + zGeoRot * zGeoRot));
+            dist = Mathf.Sqrt((float) (xGeo * xGeo + yGeoRot * yGeoRot + zGeoRot * zGeoRot));
 
-
-
-            coords = new Coords(lon, dec);
+            
+            coords = new Coords(ra, dec);
         }
         
     }
@@ -266,6 +444,13 @@ public class SolarSystemCalculations : MonoBehaviour {
         return 3.82394 * Mathf.Pow(10, -5) * (365.2422 * (epoch - 2000.0) - d);
     }
     
+    static Vector3 sph2Cart (double ra, double dec, double r) // takes ra and dec and returns a cartesian vector
+    {
+        double z = r * Mathf.Cos((float)D2R(ra)) * Mathf.Cos((float)D2R(dec));
+        double x = r * Mathf.Sin((float)D2R(ra)) * Mathf.Cos((float)D2R(dec));
+        double y = r * Mathf.Sin((float)D2R(dec));
+        return new Vector3((float)x, (float)y, (float)z);
+    }
 
 	// Update is called once per frame
 	void Update () {
