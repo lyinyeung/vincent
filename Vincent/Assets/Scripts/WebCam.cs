2196940fc85285da05786aae3f5f5b975fa77fc0
lai yin yeung <dvd042002@gmail.com>
2017-06-26 08:51:44 +0100

    Added AR camera mode

---
+++ Vincent/Assets/Scripts/WebCam.cs
@@ -0,0 +1,16 @@
+﻿// Starts the default camera and assigns the texture to the current renderer
+using UnityEngine;
+using System.Collections;
+
+public class WebCam : MonoBehaviour
+{
+    void Start()
+    {
+        Application.RequestUserAuthorization(UserAuthorization.WebCam);
+
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+}
